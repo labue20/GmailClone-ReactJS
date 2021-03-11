78168e2e1909a93b88f8c76ef554158b4b05dfb9
labue20 <labuewilfred@gmail.com>
2021-03-10 21:51:00 -0600

    commit

--- gmailclone-reactjs/src/component/layout/Header.js
+++ gmailclone-reactjs/src/component/layout/Header.js
@@ -3,23 +3,40 @@ import React from 'react'
 import styled from 'styled-components';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import SettingsIcon from '@material-ui/icons/Settings';
+import AppsIcon from '@material-ui/icons/Apps';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
 
 function Header() {
     return (
         <Wrapper>
             <LogoWrapper>
-                <div>
-                    <MenuIcon/>
-                </div>
-                <img src="https://cdn.vox-cdn.com/thumbor/8fWz6qpiMYMsZhY4vrc9Vhl5yL8=/0x110:1320x770/fit-in/1200x600/cdn.vox-cdn.com/uploads/chorus_asset/file/21939811/newgmaillogo.jpg"/>
+            
+                    <Menu>
+                        <MenuIcon/>  
+                    </Menu>
+                    <Logo>
+                        <img src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_2x_r2.png"/>
+                    </Logo>
             </LogoWrapper>
+
             <SearchWrapper>
                 <SearchBarWrapper>
-                    <SearchIcon/>
-                    <input type='text' placeholder='Search Mail'/>
-                   
+                        <SearchIcon/>
+                        <input type='text' placeholder='Search Mail'/>
+                        <ExpandMoreIcon/>
                 </SearchBarWrapper>
             </SearchWrapper>
+
+                    <IconsWrapper>
+                         <HelpOutlineIcon/>
+                         <SettingsIcon/>
+                         <AppsIcon/>
+                         <AccountCircleIcon/>
+                    </IconsWrapper>
+            
         </Wrapper>
     )
 }
@@ -27,11 +44,57 @@ function Header() {
 export default Header
 
 const Wrapper = styled.div`
-    background-color: orange;
-    height:100px;
+    display: grid;
+    grid-template-columns: 27% auto 17%;
+    border-bottom: 1px solid lightgray;
+    height: 70px;
+    align-items: center;
 `
 const LogoWrapper = styled.div`
+    height: 45px;
+    display: grid;
+    grid-template-columns: 25% auto:
+    
 `
-const SearchWrapper = styled.div``
+const Logo = styled.div`
+    display: flex;
+    height: 45px;
+    
+    
+`
+const Menu = styled.div`
+    display: grid;
+    place-items: center;
+    
 
-const SearchBarWrapper = styled.div``
+`
+const SearchWrapper = styled.div`
+
+   
+`
+
+const SearchBarWrapper = styled.div`
+    background-color: #f1f3f4;
+    width: 100%;
+    max-width: 750px;
+    display: grid:
+    grid-template-columns: 10% auto 7%;
+    place-items: center;
+    height: 45px;
+    border-radius: 6px;
+
+    input {
+        width: 100%;
+        height: 30px;
+        background: none;
+        border: none;
+        font-size: 18px;
+
+        :focus {
+            outline: none;
+        }
+    }
+`
+const IconsWrapper = styled.div`
+
+`
