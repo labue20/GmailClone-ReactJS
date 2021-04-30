1fe451dc15002534524d10346180f08871cb0b24
labue20 <labuewilfred@gmail.com>
2021-04-30 10:02:34 -0500

    work on emails view

--- gmailclone-reactjs/src/component/layout/EmailsView.js
+++ gmailclone-reactjs/src/component/layout/EmailsView.js
@@ -1,12 +1,45 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import CheckBox from '@material-ui/core/CheckBox';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import RefreshIcon from '@material-ui/icons/Refresh';
+import IconButton from '@material-ui/core/IconButton';
 
 const EmailsView = () => {
     return (
-        <div>
+        <Wrapper>
+            <TopWrapper>
+                <CheckBox/>
+
+                    <IconButton>
+                        <RefreshIcon/>
+                    </IconButton>
+
+                    <IconButton>
+                        <MoreVertIcon/>
+                    </IconButton>
+                
+            </TopWrapper>
+
+            <EmailsContainer>
             
-        </div>
+            
+            </EmailsContainer>
+        
+        </Wrapper>
     )
 }
 
 export default EmailsView
 
+const Wrapper = styled.div`
+
+`
+const TopWrapper = styled.div`
+    padding-left: 20px;
+    height: 48px;
+`
+
+const  EmailsContainer = styled.div`
+
+`
